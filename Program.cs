2da44614ebf797cc68ce1c159467186068350c4f
Kristina <kristina@1234s-MacBook-Air.local>
2022-12-09 16:17:22 +0300

    Add task

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿string[] Vvod()
+{
+    Console.WriteLine("Сколько элементов будет в массиве?");
+    int count = Convert.ToInt32(Console.ReadLine());
+    string[] array = new string[count];
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine("Ведите слово, число, символ, сроку");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+string[] NewMethod(string[] array)
+{
+    int i = 0;
+    string[] array2 = new string[array.Length];
+    int k = 0;
+    while (i < array.Length)
+    {
+        if (array[i].Length <= 3)
+        {
+            array2[k] = array[i];
+            k++;
+        }
+        i++;
+    }
+    return array2;
+}
+Console.WriteLine(string.Join(' ', NewMethod(Vvod())));
